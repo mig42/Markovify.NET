0f0df6aecabff0e44bfba2f858851a81814be97a
Miguel González <migueljgonz@gmail.com>
2020-03-01 14:07:44 +0100

    Leverage BuildHelper to manage build paths

--- build/psake.ps1
+++ build/psake.ps1
@@ -1,26 +1,49 @@
-$root = "$PSScriptRoot/.."
+$projectName = $env:BHProjectName
+$projectPath = $env:BHProjectPath
+
+$paths = @{
+  Root = $projectPath
+  Output = Join-Path -Path $projectPath -ChildPath "artifacts"
+  Library = Join-Path -Path $projectPath -ChildPath "src" -AdditionalChildPath $projectName
+  Tests = Join-Path -Path $projectPath -ChildPath "tests" -AdditionalChildPath "$projectName.Tests"
+  ModuleInfo = Join-Path -Path $projectPath -ChildPath "src" -AdditionalChildPath "$projectName.psd1"
+}
+
+function Get-CsprojName {
+  param (
+    
+  )
+  $dirName = (Get-Item $PWD).Name
+  "$dirName.csproj"
+}
+
+$moduleInfo = Import-PowerShellDataFile $paths.ModuleInfo
 
 Task default -depends Test
 
 Task Test -description "Run the unit tests" {
-  Exec -workingDirectory "$root/tests/Markovify.NET.Tests" -cmd {
-    dotnet test Markovify.NET.Tests.csproj
+  Exec -workingDirectory "$($paths.Tests)" -cmd {
+    dotnet test "$(Get-CsprojName)"
   }
 }
 
 Task Build -description "Build the module artifacts" {
-  Exec -workingDirectory "$root/src/Markovify.NET" -cmd {
-    dotnet build Markovify.NET.csproj
+  $buildNumber = if ($env:BHBuildNumber) { $env:BHBuildNumber } else { $moduleInfo.ModuleVersion }
+
+  Exec -workingDirectory "$($paths.Library)" -cmd {
+    dotnet build "$(Get-CsprojName)" `
+      -o $paths.Output `
+      /p:Author=$moduleInfo.Author `
+      /p:Version=$buildNumber
   }
 }
 
 Task Clean -description "Clean built artifacts" {
-  Exec -workingDirectory "$root/src/Markovify.NET" -cmd {
-    dotnet build Markovify.NET.csproj
+  Exec -workingDirectory $paths.Root -cmd {
+    dotnet clean "$($paths.Root)/$projectName.sln" -o $paths.Output
   }
 }
 
-task Publish -description "Publish the module to the NuGet Gallery" `
-    -depends Clean, Build {
+Task Publish -depends Clean, Build -description "Publish the module to the NuGet Gallery" {
   "Publish"
 }
