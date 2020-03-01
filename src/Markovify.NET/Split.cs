7f4e017c9a61e03b01b92fb2dcb02d1601ebc8e6
Miguel González <migueljgonz@gmail.com>
2020-03-01 23:39:47 +0100

    Reorganize constants into classes

--- src/Markovify.NET/Split.cs
+++ src/Markovify.NET/Split.cs
@@ -9,7 +9,7 @@ namespace Markovify.NET
     {
         internal static IEnumerable<string> IntoSentences(string inputText)
         {
-            IEnumerable<int> endIndices = mEndPatterns.Matches(inputText)
+            IEnumerable<int> endIndices = RegularExpressions.EndPatterns.Matches(inputText)
                 .OfType<Match>()
                 .Where(match => IsSentenceEnder(match.Groups[1].Value))
                 .Select(match => match.Index + match.Groups[1].Length + match.Groups[2].Length);
@@ -19,16 +19,21 @@ namespace Markovify.NET
                 (start, end) => inputText.Substring(start, end - start));
         }
 
+        internal static IEnumerable<string> IntoWords(string sentence)
+        {
+            return RegularExpressions.WordSplit.Split(sentence);
+        }
+
         static bool IsSentenceEnder(string word)
         {
-            if (mExceptions.Contains(word))
+            if (Abbreviations.Exceptions.Contains(word))
                 return false;
 
             char lastChar = word.Last();
             if (lastChar == '?' || lastChar == '!')
                 return true;
 
-            if (mNotStartUppercase.Replace(word, string.Empty).Length > 1)
+            if (RegularExpressions.NotStartUppercase.Replace(word, string.Empty).Length > 1)
                 return true;
 
             return lastChar == '.' && !IsAbbreviation(word);
@@ -37,49 +42,60 @@ namespace Markovify.NET
         static bool IsAbbreviation(string word)
         {
             string clipped = word.Substring(0, word.Length - 1);
-            if (ASCII_UPPERCASE.Contains(clipped[0]))
-                return mAbbrCapped.Contains(clipped.ToLowerInvariant());
+            if (Abbreviations.AsciiUppercase.Contains(clipped[0]))
+                return Abbreviations.Capped.Contains(clipped.ToLowerInvariant());
 
-            return mAbbrLowercase.Contains(clipped);
+            return Abbreviations.Lowercase.Contains(clipped);
         }
 
-        static readonly Regex mEndPatterns = new Regex(
-            string.Concat(
-                @"([\w\.'’&\]\)]+[\.\?!])", // a word that ends with punctuation
-                @"([‘’“”'""\)\]]*)", // Followed by optional quote/parens/etc
-                @"(\s+(?![a-z\-–—]))" // Followed by whitespace + non-(lowercase or dash)
-            ),
-            RegexOptions.Compiled);
-
-        static readonly Regex mNotStartUppercase = new Regex(@"[^A-Z]", RegexOptions.Compiled);
-
-        static HashSet<string> mExceptions = new HashSet<string>
+        static class RegularExpressions
         {
-            "U.S.",
-            "U.N.",
-            "E.U.",
-            "F.B.I.",
-            "C.I.A."
-        };
+            internal static readonly Regex EndPatterns = new Regex(
+                string.Concat(
+                    @"([\w\.'’&\]\)]+[\.\?!])", // a word that ends with punctuation
+                    @"([‘’“”'""\)\]]*)", // Followed by optional quote/parens/etc
+                    @"(\s+(?![a-z\-–—]))" // Followed by whitespace + non-(lowercase or dash)
+                ),
+                RegexOptions.Compiled);
 
-        const string ASCII_LOWERCASE = "abcdefghijklmnnopqrstuvwxyz";
-        static readonly string ASCII_UPPERCASE = ASCII_LOWERCASE.ToUpperInvariant();
+            internal static readonly Regex NotStartUppercase = new Regex(
+                @"[^A-Z]", RegexOptions.Compiled);
 
-        static readonly HashSet<string> mAbbrLowercase = new HashSet<string>
+            internal static readonly Regex WordSplit = new Regex(
+                @"\s+", RegexOptions.Compiled);
+        }
+
+        static class Abbreviations
         {
-            "etc", "v", "vs", "viz", "al", "pct"
-        };
+            internal static HashSet<string> Exceptions = new HashSet<string>
+            {
+                "U.S.",
+                "U.N.",
+                "E.U.",
+                "F.B.I.",
+                "C.I.A."
+            };
 
-        // States w/ with thanks to https://github.com/unitedstates/python-us
-        const string STATES =
-            "ala|ariz|ark|calif|colo|conn|del|fla|ga|ill|ind|kan|ky|la|md|mass|mich|minn|miss|mo|mont|neb|nev|okla|ore|pa|tenn|vt|va|wash|wis|wyo";
-        // Titles w/ thanks to https://github.com/nytimes/emphasis and @donohoe
-        const string TITLES =
-            "mr|ms|mrs|msr|dr|gov|pres|sen|sens|rep|reps|prof|gen|messrs|col|sr|jf|sgt|mgr|fr|rev|jr|snr|atty|supt";
-        const string STREETS = "ave|blvd|st|rd|hwy";
-        const string MONTHS = "jan|feb|mar|apr|jun|jul|aug|sep|sept|oct|nov|dec";
-        static readonly HashSet<string> mAbbrCapped = new HashSet<string>(
-            string.Join("|", STATES, TITLES, STREETS, MONTHS).Split('|')
-        );
+            internal const string AsciiLowercase = "abcdefghijklmnnopqrstuvwxyz";
+            internal static readonly string AsciiUppercase = AsciiLowercase.ToUpperInvariant();
+
+            internal static readonly HashSet<string> Lowercase = new HashSet<string>
+            {
+                "etc", "v", "vs", "viz", "al", "pct"
+            };
+
+            internal static readonly HashSet<string> Capped = new HashSet<string>(
+                string.Join("|", STATES, TITLES, STREETS, MONTHS).Split('|')
+            );
+
+            // States w/ with thanks to https://github.com/unitedstates/python-us
+            const string STATES =
+                "ala|ariz|ark|calif|colo|conn|del|fla|ga|ill|ind|kan|ky|la|md|mass|mich|minn|miss|mo|mont|neb|nev|okla|ore|pa|tenn|vt|va|wash|wis|wyo";
+            // Titles w/ thanks to https://github.com/nytimes/emphasis and @donohoe
+            const string TITLES =
+                "mr|ms|mrs|msr|dr|gov|pres|sen|sens|rep|reps|prof|gen|messrs|col|sr|jf|sgt|mgr|fr|rev|jr|snr|atty|supt";
+            const string STREETS = "ave|blvd|st|rd|hwy";
+            const string MONTHS = "jan|feb|mar|apr|jun|jul|aug|sep|sept|oct|nov|dec";
+        }
     }
 }
