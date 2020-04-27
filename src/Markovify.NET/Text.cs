1488a8d62e2e1a376e2c2f1e020f9c286358a487
Miguel González <migueljgonz@gmail.com>
2020-04-27 20:50:21 +0200

    Change Text properties and Build() method.
    It no longer makes sense to keep the state size. Maybe I'll need to save it later, but for now it has to go.

--- src/Markovify.NET/Text.cs
+++ src/Markovify.NET/Text.cs
@@ -10,38 +10,8 @@ namespace Markovify.NET
     /// <summary>
     /// A source text to process using Markov chains.
     /// </summary>
-    /// <param name="inputText">
-    ///   The source text.
-    /// </param>
-    /// <param name="stateSize">
-    ///   The number of words in the model's state
-    /// </param>
-    /// <param name="chain">
-    ///   A trained <see cref="Chain" /> for this text, if pre-processed.
-    /// </param>
-    /// <param name="parsedSentences">
-    ///   A list of lists, where each outer list is a "run" of the process (i.e. a single sentence),
-    ///   and each inner list contains the steps (i.e. words) in the run.
-    ///   If you want to simulate an infinite process, you can come very close by passing just one,
-    ///   very long run.
-    /// </param>
-    /// <param name="retainOriginal">
-    ///   Whether to keep the original corpus.
-    /// </param>
-    /// <param name="isWellFormed">
-    ///   Indicates whether sentences should be well-formed, preventing unmatched quotes,
-    ///   parentheses by default, or a custom regular expression can be provided.
-    /// </param>
-    /// <param name="rejectExpression">
-    ///   If <paramref name="isWellFormed" /> is <c>true</c>, this can be provided to override
-    ///   the standard rejection pattern.
-    /// </param>
     public class Text
     {
-        [DefaultValue(2)]
-        [JsonProperty("stateSize")]
-        public int StateSize { get; set; } = 2;
-
         [JsonProperty("chain")]
         public Chain Chain { get; set; }
 
@@ -54,6 +24,27 @@ namespace Markovify.NET
         [JsonProperty("rejectExpression")]
         public Regex RejectExpression { get; set; } = DefaultRejectExpression;
 
+        /// <summary>
+        /// Build the text from its source.
+        /// </summary>
+        /// <param name="inputText">
+        ///   The source text.
+        /// </param>
+        /// <param name="stateSize">
+        ///   The number of words in the model's state
+        /// </param>
+        /// <param name="retainOriginal">
+        ///   Whether to keep the original corpus.
+        /// </param>
+        /// <param name="isWellFormed">
+        ///   Indicates whether sentences should be well-formed, preventing unmatched quotes,
+        ///   parentheses by default, or a custom regular expression can be provided.
+        /// </param>
+        /// <param name="rejectExpression">
+        ///   If <paramref name="isWellFormed" /> is <c>true</c>, this can be provided to override
+        ///   the standard rejection pattern.
+        /// </param>
+        /// <returns></returns>
         public static Text Build(
             string inputText,
             int stateSize = 2,
@@ -61,46 +52,24 @@ namespace Markovify.NET
             bool isWellFormed = true,
             Regex rejectExpression = null)
         {
+            if (stateSize < 1)
+                return Empty;
+            
             if (rejectExpression == null && isWellFormed)
                 rejectExpression = DefaultRejectExpression;
 
-            IEnumerable<IEnumerable<string>> corpus = GenerateCorpus(
+            var corpus = GenerateCorpus(
                 inputText, isWellFormed ? rejectExpression : null);
 
+            var parsedSentences = corpus.ToList();
             return new Text
             {
-                Chain = new Chain(corpus, stateSize),
-                StateSize = stateSize,
+                Chain = Chain.FromSentences(parsedSentences, stateSize),
                 IsWellFormed = isWellFormed,
                 RejectExpression = rejectExpression,
-                SourceText = retainOriginal && corpus.Count() > 0 && !string.IsNullOrEmpty(inputText)
-                    ? string.Join(" ", corpus.Select(words => string.Join(" ", words)))
-                    : null
-            };
-        }
-
-        /// <summary>
-        ///   Compiles the text model for improved text generation speed and reduced size.
-        /// </summary>
-        /// <param name="inPlace">
-        ///   Whether to compile the current instance or create a new instance.
-        /// </param>
-        /// <returns>A compiled instance of <see cref="Text" /></returns>
-        public Text Compile(bool inPlace = false)
-        {
-            if (inPlace)
-            {
-                Chain.Compile(true);
-                return this;
-            }
-
-            return new Text
-            {
-                Chain = Chain.Compile(false),
-                StateSize = StateSize,
-                SourceText = SourceText,
-                IsWellFormed = IsWellFormed,
-                RejectExpression = RejectExpression,
+                SourceText = retainOriginal && parsedSentences.Any() && !string.IsNullOrEmpty(inputText)
+                    ? string.Join(" ", parsedSentences.Select(words => string.Join(" ", words)))
+                    : string.Empty
             };
         }
 
@@ -109,7 +78,8 @@ namespace Markovify.NET
         ///   each of which is a list of words. Before splitting into words, the sentences
         ///   are filtered through <see cref="TestSentenceInput" />
         /// </summary>
-        /// <param name="inputText">The sourcce text</param>
+        /// <param name="inputText">The source text</param>
+        /// <param name="rejectExpression">The expression to reject</param>
         /// <returns>The corpus as a list of lists.</returns>
         static IEnumerable<IEnumerable<string>> GenerateCorpus(
             string inputText, Regex rejectExpression)
@@ -140,5 +110,12 @@ namespace Markovify.NET
 
         static readonly Regex DefaultRejectExpression = new Regex(
             @"(^')|('$)|\s'|'\s|[""(\(\)\[\])]", RegexOptions.Compiled);
+
+        static readonly Text Empty = new Text
+        {
+            Chain = Chain.Empty,
+            RejectExpression = DefaultRejectExpression,
+            SourceText = string.Empty
+        };
     }
 }
