19b27c02b73e41642d9506aec94fe5cc4e121c46
Miguel González <migueljgonz@gmail.com>
2020-06-07 19:22:34 +0200

    Add two new chain tests. Also check model size.

--- tests/Markovify.NET.Tests/ChainTests.cs
+++ tests/Markovify.NET.Tests/ChainTests.cs
@@ -1,12 +1,11 @@
-using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
+using System.Linq;
 using FluentAssertions;
 using Xunit;
 
 namespace Markovify.NET.Tests
 {
-    public class ChainTests
+    public static class ChainTests
     {
         public class Build
         {
@@ -18,8 +17,9 @@ namespace Markovify.NET.Tests
             [InlineData(2)]
             public void EmptyList_ShouldReturnEmpty(int stateSize)
             {
-                var text = Chain.FromSentences(new List<List<string>>(), stateSize);
-                text.Should().Be(Text.Empty);
+                var chain = Chain.FromSentences(new List<List<string>>(), stateSize);
+                chain.Should().Be(Chain.Empty);
+                chain.ModelSize.Should().Be(0);
             }
 
             [Theory]
@@ -29,26 +29,60 @@ namespace Markovify.NET.Tests
             public void StateSizeLessThan1_ShouldReturnEmpty(int stateSize)
             {
                 var corpus = new List<List<string>> { new List<string> { "Sample", "sentence." } };
-                var text = Chain.FromSentences(corpus, stateSize);
+                var chain = Chain.FromSentences(corpus, stateSize);
 
-                text.Should().Be(Text.Empty);
+                chain.Should().Be(Chain.Empty);
+                chain.ModelSize.Should().Be(0);
             }
 
             [Theory]
-            [InlineData(0)]
             [InlineData(1)]
             [InlineData(2)]
             public void ValidData_ShouldReturnValidChain(int stateSize)
             {
                 var corpus = new List<List<string>> { new List<string> { "Sample", "sentence." } };
-                var text = Chain.FromSentences(corpus, stateSize);
+                var chain = Chain.FromSentences(corpus, stateSize);
 
-                text.Should().NotBe(Text.Empty);
+                chain.Should().NotBe(Chain.Empty);
+                chain.ModelSize.Should().Be(3);
             }
         }
 
         public class GenerateSentence
         {
+            [Theory]
+            [InlineData(1)]
+            [InlineData(2)]
+            [InlineData(3)]
+            public void SingleSentence_ShouldReturnTheSame(int stateSize)
+            {
+                List<string> expectedSentence = new List<string> {"This", "is", "a", "sentence."};
+                Chain chain = Chain.FromSentences(
+                    new List<List<string>> { expectedSentence },
+                    stateSize);
+
+                List<string> actualSentence = chain.GenerateSentence();
+
+                chain.ModelSize.Should().Be(expectedSentence.Count + 1);
+                actualSentence.Should().ContainInOrder(expectedSentence);
+            }
+
+            [Fact]
+            public void TwoSentencesWithDifferentLastWord_ShouldBuildOneOrTheOther()
+            {
+                List<string> sentence1 = new List<string> {"This", "is", "a", "sentence."};
+                List<string> sentence2 = sentence1.SkipLast(1).Append("sample.").ToList();
+
+                Chain chain = Chain.FromSentences(
+                    new List<List<string>> { sentence1, sentence2 },
+                    1);
+
+                List<string> actualSentence = chain.GenerateSentence();
+
+                chain.ModelSize.Should().Be(sentence1.Count + 2);
+                actualSentence.SkipLast(1).Should().ContainInOrder(sentence1.SkipLast(1));
+                actualSentence.Last().Should().BeOneOf(sentence1.TakeLast(1).Concat(sentence2.TakeLast(1)));
+            }
         }
     }
 }
